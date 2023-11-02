7a117ef26395beb8862263ae099dafc39b51fbb3
ValerioCarbone <valeriocarbone10@gmail.com>
2023-11-02 16:42:01 +0100

    CHANGE - Code spacing

--- js/app.js
+++ js/app.js
@@ -2,23 +2,24 @@
 const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
-            emailsArray:[]
+    data() {
+        return {
+            emailsArray: []
         }
     },
-    methods:{
-        getEmails(index){
-           for(let i = 1; i <= index; i++){
-            axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((res) => {   
-                this.emailsArray.push(res.data.response)
-            })
-           }
+    methods: {
+        getEmails(index) {
+            for (let i = 1; i <= index; i++) {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((res) => {
+                        this.emailsArray.push(res.data.response)
+                    })
+            }
+
         }
     },
-    beforeMount(){
+    beforeMount() {
         this.getEmails(10)
     },
 }).mount('#app')
