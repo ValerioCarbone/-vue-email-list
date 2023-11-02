bae0c9724f1ee944c9e9569c39aca311f7d521f4
ValerioCarbone <valeriocarbone10@gmail.com>
2023-11-02 15:58:13 +0100

    ADD - Files + links + emailGenerator by API

---
+++ js/app.js
@@ -0,0 +1,28 @@
+console.log('hello')
+
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            emailsArray:[],
+            currentEmail:''
+        }
+    },
+    methods:{
+        getEmails(index){
+           for(let i = 1; i <= index; i++){
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res) => {   
+                this.emailsArray.push(res.data.response)
+            })
+           }
+        }
+    },
+    beforeMount(){
+        this.getEmails(10)
+    },
+}).mount('#app')
+
+
